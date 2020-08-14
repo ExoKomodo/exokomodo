aeb0fd6a41d823a688badda8b99bf9f9702e69dd
James Orson <jamesaorson@gmail.com>
2020-08-14 00:28:01 -0700

    Add AppState and track changes in state

---
+++ Config/AppState.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace ExoKomodo.Config
+{
+    public static class AppState
+    {
+        #region Public
+
+        #region Members
+        public static bool IsSidebarHidden
+        {
+            get => _isSidebarHidden;
+            set
+            {
+                _isSidebarHidden = value;
+                NotifyStateChanged();
+            }
+        }
+        public static event Action OnChange;
+        #endregion
+
+        #endregion
+
+        #region Private
+
+        #region Members
+        private static bool _isSidebarHidden { get; set; }
+        #endregion
+
+        #region Member Methods
+        private static void NotifyStateChanged() => OnChange?.Invoke();
+        #endregion
+
+        #endregion
+    }
+}
---
+++ Shared/MainLayout.razor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ExoKomodo.Config;
+
+namespace ExoKomodo.Shared
+{
+    public partial class MainLayout : IDisposable
+    {
+        #region Public
+
+        #region Member Methods
+        public void Dispose()
+        {
+            AppState.OnChange -= StateHasChanged;
+        }
+        #endregion
+
+        #endregion
+
+        #region Protected
+
+        #region Member Methods
+        protected override void OnInitialized()
+        {
+            AppState.OnChange += StateHasChanged;
+        }
+        #endregion
+
+        #endregion
+    }
+}
