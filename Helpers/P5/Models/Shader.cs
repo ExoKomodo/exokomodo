7dec1f0c34e58e5b26d18db500e3907ccb504fc2
JamesOrson <james.osborne112@gmail.com>
2020-08-31 00:07:35 -0700

    Add P5 shader support

---
+++ Helpers/P5/Models/Shader.cs
@@ -0,0 +1,15 @@
+using System.Numerics;
+
+namespace ExoKomodo.Helpers.P5.Models
+{
+    public class Shader
+    {
+        #region Public
+
+        #region Members
+        public string Id { get; set; }
+        #endregion
+
+        #endregion
+    }
+}
