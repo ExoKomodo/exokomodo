a58fe5933bf6ff790b2ae3e38aba5fc22ea03781
JamesOrson <james.osborne112@gmail.com>
2020-08-31 00:11:18 -0700

    Add P5 RandomGaussian

---
+++ Helpers/P5/P5App.Math.Random.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ExoKomodo.Helpers.P5
+{
+    public abstract partial class P5App
+    {
+        #region Public
+
+        #region Member Methods
+        public float RandomGaussian(float mean, float standardDeviation) => _jsRuntime.Invoke<float>(
+            _p5InvokeFunctionAndReturn,
+            "randomGaussian",
+            mean,
+            standardDeviation
+        );
+        #endregion
+
+        #endregion
+    }
+}
