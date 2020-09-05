4afba40df857dba1ced0c022170f28fa7402d063
James Orson <jamesaorson@gmail.com>
2020-09-04 19:22:59 -0700

    Add P5 SoundFile support

---
+++ Helpers/P5/Models/Sound.cs
@@ -0,0 +1,15 @@
+using System.Numerics;
+
+namespace ExoKomodo.Helpers.P5.Models
+{
+    public class Sound
+    {
+        #region Public
+
+        #region Members
+        public string Id { get; set; }
+        #endregion
+
+        #endregion
+    }
+}
---
+++ Helpers/P5/P5App.Sound.cs
@@ -0,0 +1,9 @@
+using System;
+using ExoKomodo.Helpers.P5.Enums;
+using ExoKomodo.Helpers.P5.Models;
+using Microsoft.JSInterop;
+
+namespace ExoKomodo.Helpers.P5
+{
+    public abstract partial class P5App {}
+}
