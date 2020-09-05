a8d10bc71e52f315d6719f0419ff9bdab80ddceb
James Orson <jamesaorson@gmail.com>
2020-09-04 18:37:35 -0700

    Reorganize p5Wrapper

--- wwwroot/js/p5Wrapper.js
+++ wwwroot/js/p5Wrapper.js
@@ -3,21 +3,7 @@ function startP5(p5Implementation, container) {
     let sketch = function(p) {
         window.p5Instance = p;
 
-        p.nextCameraId = 0;
-
-        p.cameras = {};
-        p.fonts = {};
-        p.images = {};
-        p.models = {};
-        p.shaders = {};
-
-        p.createCameraDotnet = function() {
-            p.cameras[p.nextCameraId] = this.createCamera();
-            return {
-                id: p.nextCameraId++,
-            };
-        }
-
+        // #region Main functions
         p.deviceMoved = function() {
             p5Implementation.invokeMethod('deviceMoved');
         }
@@ -42,63 +28,8 @@ function startP5(p5Implementation, container) {
             p5Implementation.invokeMethod('draw');
         }
 
-        p.fontDotnet = function(id, size) {
-            let font = this.fonts[id];
-            if (!font) {
-                console.error(`Font with id '${id}' is not loaded`);
-                return;
-            }
-            if (!size) {
-                this.textFont(font);
-            } else {
-                this.textFont(font, size);
-            }
-        }
-
-        p.frustumDotnet = function(id, left, right, bottom, top, near, far) {
-            let camera = this.cameras[id];
-            if (!camera) {
-                console.error(`Camera with id '${id}' is not loaded`);
-                return;
-            }
-            camera.frustum(left, right, bottom, top, near, far);
-        }
-
         p.getValue = function(valueName) {
             return this[valueName];
-        }
-
-        p.imageDotnet = function(id, x, y, width, height) {
-            let image = this.images[id];
-            if (!image) {
-                console.error(`Image with id '${id}' is not loaded`);
-                return;
-            }
-            this.image(
-                image,
-                x,
-                y,
-                width,
-                height
-            );
-        }
-
-        p.imageHeightDotnet = function(id) {
-            let image = this.images[id];
-            if (!image) {
-                console.error(`Image with id '${id}' is not loaded`);
-                return;
-            }
-            return image.height;
-        }
-
-        p.imageWidthDotnet = function(id) {
-            let image = this.images[id];
-            if (!image) {
-                console.error(`Image with id '${id}' is not loaded`);
-                return;
-            }
-            return image.width;
         }
         
         p.invokeP5Function = function(functionName) {
@@ -123,74 +54,6 @@ function startP5(p5Implementation, container) {
             return p5Implementation.invokeMethod('keyTyped');
         }
 
-        p.loadFontDotnet = function(fontPath) {
-            if (!this.fonts[fontPath])
-            {
-                this.fonts[fontPath] = this.loadFont(fontPath);
-            }
-            return {
-                id: fontPath,
-            };
-        }
-        
-        p.loadImageDotnet = function(imagePath) {
-            if (!this.images[imagePath])
-            {
-                this.images[imagePath] = this.loadImage(imagePath);
-            }
-            return {
-                id: imagePath,
-            };
-        }
-
-        p.loadModelDotnet = function(modelPath, normalize) {
-            if (!this.models[modelPath])
-            {
-                this.models[modelPath] = this.loadModel(modelPath, normalize);
-            }
-            return {
-                id: modelPath,
-            };
-        }
-
-        p.loadPixelsDotnet = function(id) {
-            let image = this.images[id];
-            if (!image) {
-                console.error(`Image with id '${id}' is not loaded`);
-                return;
-            }
-            image.loadPixels();
-        }
-
-        p.loadShaderDotnet = function(vertexShaderPath, fragmentShaderPath) {
-            let id = `${vertexShaderPath}-${fragmentShaderPath}`;
-            if (!this.shaders[id])
-            {
-                this.shaders[id] = this.loadShader(vertexShaderPath, fragmentShaderPath);
-            }
-            return {
-                id,
-            };
-        }
-
-        p.lookAtDotnet = function(id, x, y, z) {
-            let camera = this.cameras[id];
-            if (!camera) {
-                console.error(`Camera with id '${id}' is not loaded`);
-                return;
-            }
-            camera.lookAt(x, y, z);
-        }
-
-        p.modelDotnet = function(id) {
-            let model = this.models[id];
-            if (!model) {
-                console.error(`Model with id '${id}' is not loaded`);
-                return;
-            }
-            this.model(model);
-        }
-
         p.mouseClicked = function() {
             return p5Implementation.invokeMethod('mouseClicked');
         }
@@ -213,6 +76,60 @@ function startP5(p5Implementation, container) {
 
         p.mouseWheel = function(event) {
             return p5Implementation.invokeMethod('mouseWheel', event.delta);
+        }
+
+        p.preload = function() {
+            p5Implementation.invokeMethod('preload');
+        }
+        
+        p.setup = function() {
+            p5Implementation.invokeMethod('setup');
+        }
+
+        p.touchEnded = function() {
+            return p5Implementation.invokeMethod('touchEnded');
+        }
+
+        p.touchMoved = function() {
+            return p5Implementation.invokeMethod('touchMoved');
+        }
+
+        p.touchStarted = function() {
+            return p5Implementation.invokeMethod('touchStarted');
+        }
+
+        p.windowResized = function() {
+            p5Implementation.invokeMethod('windowResized');
+        }
+        // #endregion
+
+        // #region Camera
+        p.cameras = {};
+        p.nextCameraId = 0;
+        
+        p.createCameraDotnet = function() {
+            p.cameras[p.nextCameraId] = this.createCamera();
+            return {
+                id: p.nextCameraId++,
+            };
+        }
+
+        p.frustumDotnet = function(id, left, right, bottom, top, near, far) {
+            let camera = this.cameras[id];
+            if (!camera) {
+                console.error(`Camera with id '${id}' is not loaded`);
+                return;
+            }
+            camera.frustum(left, right, bottom, top, near, far);
+        }
+
+        p.lookAtDotnet = function(id, x, y, z) {
+            let camera = this.cameras[id];
+            if (!camera) {
+                console.error(`Camera with id '${id}' is not loaded`);
+                return;
+            }
+            camera.lookAt(x, y, z);
         }
 
         p.moveDotnet = function(id, x, y, z) {
@@ -251,10 +168,6 @@ function startP5(p5Implementation, container) {
             camera.perspective(fovY, aspect, near, far);
         }
 
-        p.preload = function() {
-            p5Implementation.invokeMethod('preload');
-        }
-
         p.setCameraDotnet = function(id) {
             let camera = this.cameras[id];
             if (!camera) {
@@ -282,26 +195,40 @@ function startP5(p5Implementation, container) {
             camera.setPosition(x, y, z);
         }
 
-        p.setUniformDotnet = function(id, uniformName, value) {
-            let shader = this.shaders[id];
-            if (!shader) {
-                console.error(`Shader with id '${id}' is not loaded`);
+        p.tiltDotnet = function(id, angle) {
+            let camera = this.cameras[id];
+            if (!camera) {
+                console.error(`Camera with id '${id}' is not loaded`);
                 return;
             }
-            shader.setUniform(uniformName, value);
+            camera.tilt(angle);
         }
-        
-        p.setup = function() {
-            p5Implementation.invokeMethod('setup');
-        }
-        
-        p.shaderDotnet = function(id) {
-            let shader = this.shaders[id];
-            if (!shader) {
-                console.error(`Shader with id '${id}' is not loaded`);
+        // #endregion
+
+        // #region Font
+        p.fonts = {};
+
+        p.fontDotnet = function(id, size) {
+            let font = this.fonts[id];
+            if (!font) {
+                console.error(`Font with id '${id}' is not loaded`);
                 return;
             }
-            this.shader(shader);
+            if (!size) {
+                this.textFont(font);
+            } else {
+                this.textFont(font, size);
+            }
+        }
+
+        p.loadFontDotnet = function(fontPath) {
+            if (!this.fonts[fontPath])
+            {
+                this.fonts[fontPath] = this.loadFont(fontPath);
+            }
+            return {
+                id: fontPath,
+            };
         }
 
         p.textFontDotnet = function(id, size) {
@@ -316,6 +243,62 @@ function startP5(p5Implementation, container) {
                 this.textFont(this.fonts[id], size);
             }
         }
+        // #endregion
+
+        // #region Image
+        p.images = {};
+
+        p.imageDotnet = function(id, x, y, width, height) {
+            let image = this.images[id];
+            if (!image) {
+                console.error(`Image with id '${id}' is not loaded`);
+                return;
+            }
+            this.image(
+                image,
+                x,
+                y,
+                width,
+                height
+            );
+        }
+
+        p.imageHeightDotnet = function(id) {
+            let image = this.images[id];
+            if (!image) {
+                console.error(`Image with id '${id}' is not loaded`);
+                return;
+            }
+            return image.height;
+        }
+
+        p.imageWidthDotnet = function(id) {
+            let image = this.images[id];
+            if (!image) {
+                console.error(`Image with id '${id}' is not loaded`);
+                return;
+            }
+            return image.width;
+        }
+
+        p.loadImageDotnet = function(imagePath) {
+            if (!this.images[imagePath])
+            {
+                this.images[imagePath] = this.loadImage(imagePath);
+            }
+            return {
+                id: imagePath,
+            };
+        }
+
+        p.loadPixelsDotnet = function(id) {
+            let image = this.images[id];
+            if (!image) {
+                console.error(`Image with id '${id}' is not loaded`);
+                return;
+            }
+            image.loadPixels();
+        }
 
         p.textureDotnet = function(id) {
             let image = this.images[id];
@@ -325,31 +308,63 @@ function startP5(p5Implementation, container) {
             }
             this.texture(image);
         }
+        // #endregion
 
-        p.tiltDotnet = function(id, angle) {
-            let camera = this.cameras[id];
-            if (!camera) {
-                console.error(`Camera with id '${id}' is not loaded`);
+        // #region Model
+        p.models = {};
+
+        p.loadModelDotnet = function(modelPath, normalize) {
+            if (!this.models[modelPath])
+            {
+                this.models[modelPath] = this.loadModel(modelPath, normalize);
+            }
+            return {
+                id: modelPath,
+            };
+        }
+
+        p.modelDotnet = function(id) {
+            let model = this.models[id];
+            if (!model) {
+                console.error(`Model with id '${id}' is not loaded`);
                 return;
             }
-            camera.tilt(angle);
+            this.model(model);
+        }
+        // #endregion
+
+        // #region Shader
+        p.shaders = {};
+
+        p.loadShaderDotnet = function(vertexShaderPath, fragmentShaderPath) {
+            let id = `${vertexShaderPath}-${fragmentShaderPath}`;
+            if (!this.shaders[id])
+            {
+                this.shaders[id] = this.loadShader(vertexShaderPath, fragmentShaderPath);
+            }
+            return {
+                id,
+            };
         }
 
-        p.touchEnded = function() {
-            return p5Implementation.invokeMethod('touchEnded');
+        p.shaderDotnet = function(id) {
+            let shader = this.shaders[id];
+            if (!shader) {
+                console.error(`Shader with id '${id}' is not loaded`);
+                return;
+            }
+            this.shader(shader);
         }
 
-        p.touchMoved = function() {
-            return p5Implementation.invokeMethod('touchMoved');
+        p.setUniformDotnet = function(id, uniformName, value) {
+            let shader = this.shaders[id];
+            if (!shader) {
+                console.error(`Shader with id '${id}' is not loaded`);
+                return;
+            }
+            shader.setUniform(uniformName, value);
         }
-
-        p.touchStarted = function() {
-            return p5Implementation.invokeMethod('touchStarted');
-        }
-
-        p.windowResized = function() {
-            p5Implementation.invokeMethod('windowResized');
-        }
+        // #endregion
     };
     new p5(sketch, document.getElementById(container));
 }
