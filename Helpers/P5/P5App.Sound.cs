ed3ec236411f1a324cfc386e28fa78103b83be05
James Orson <jamesaorson@gmail.com>
2020-09-08 17:52:46 -0700

    Fix audio start issues in P5

--- Helpers/P5/P5App.Sound.cs
+++ Helpers/P5/P5App.Sound.cs
@@ -5,5 +5,47 @@ using Microsoft.JSInterop;
 
 namespace ExoKomodo.Helpers.P5
 {
-    public abstract partial class P5App {}
+    public abstract partial class P5App
+    {
+        #region Public
+
+        #region Members
+        public float MasterVolume
+        {
+            get => _jsRuntime.Invoke<float>(
+                _p5InvokeFunctionAndReturn,
+                "getMasterVolume"
+            );
+        }
+        #endregion
+
+        #region Member Methods
+        public void SetBPM(float bpm, float rampTime) => _jsRuntime.InvokeVoid(
+            _p5InvokeFunction,
+            "setBPM",
+            bpm,
+            rampTime
+        );
+
+        public void SetMasterVolume(float volume) => _jsRuntime.InvokeVoid(
+            _p5InvokeFunction,
+            "masterVolume",
+            volume
+        );
+        
+        public void SetMasterVolume(float volume, float rampTime) => _jsRuntime.InvokeVoid(
+            _p5InvokeFunction,
+            "masterVolume",
+            volume,
+            rampTime
+        );
+
+        public void UserStartAudio() => _jsRuntime.InvokeVoid(
+            _p5InvokeFunction,
+            "userStartAudio"
+        );
+        #endregion
+
+        #endregion
+    }
 }
