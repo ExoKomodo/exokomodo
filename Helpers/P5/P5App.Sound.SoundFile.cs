9caf01d787b65c8cf936c7bae758875108583a4c
James Orson <jamesaorson@gmail.com>
2020-09-08 17:52:46 -0700

    Fix audio start issues in P5

--- Helpers/P5/P5App.Sound.SoundFile.cs
+++ Helpers/P5/P5App.Sound.SoundFile.cs
@@ -9,16 +9,6 @@ namespace ExoKomodo.Helpers.P5
     {
         #region Public
 
-        #region Members
-        public float MasterVolume
-        {
-            get => _jsRuntime.Invoke<float>(
-                _p5InvokeFunctionAndReturn,
-                "getMasterVolume"
-            );
-        }
-        #endregion
-
         #region Member Methods
         public float GetCurrentTime(
             Sound sound
@@ -116,26 +106,6 @@ namespace ExoKomodo.Helpers.P5
             amp,
             cueStart,
             duration
-        );
-
-        public void SetBPM(float bpm, float rampTime) => _jsRuntime.InvokeVoid(
-            _p5InvokeFunction,
-            "setBPM",
-            bpm,
-            rampTime
-        );
-
-        public void SetMasterVolume(float volume) => _jsRuntime.InvokeVoid(
-            _p5InvokeFunction,
-            "masterVolume",
-            volume
-        );
-        
-        public void SetMasterVolume(float volume, float rampTime) => _jsRuntime.InvokeVoid(
-            _p5InvokeFunction,
-            "masterVolume",
-            volume,
-            rampTime
         );
 
         public void SetPlayMode(Sound sound, PlayMode mode) => _jsRuntime.InvokeVoid(
