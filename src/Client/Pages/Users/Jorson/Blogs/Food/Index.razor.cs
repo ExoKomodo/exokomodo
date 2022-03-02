d67bd57563fc4e4def0bb7a66068a76dcf837540
James Orson <jamesaorson@gmail.com>
2022-03-02 15:05:43 -0800

    Add food blog for jorson

---
+++ src/Client/Pages/Users/Jorson/Blogs/Food/Index.razor.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Client.Models.Jorson;
+using Client.Services.Jorson;
+
+namespace Client.Pages.Users.Jorson.Blogs.Food
+{
+    internal class IndexBase : PageBase {}
+
+    public partial class Index
+    {
+        #region Public
+
+        #region Constructors
+        public Index()
+        {
+            _base = new IndexBase();
+        }
+        #endregion
+
+        #region Constants
+        public static string UserId = "jorson";
+        #endregion
+
+        #endregion
+
+        #region Protected
+
+        #region Member Methods
+        protected override void OnAfterRender(bool firstRender)
+        {
+            base.OnAfterRender(firstRender);
+
+            if (firstRender)
+            {
+                _base.Initialize();
+            }
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            _blogService.UserId = UserId;
+            _blogs = await _blogService.GetAsync("food/food.json");
+        }
+        #endregion
+
+        #endregion
+
+        #region Private
+
+        #region Members
+        private IEnumerable<Blog<int>> _blogs { get; set; }
+        [Inject]
+        private BlogService _blogService { get; set; }
+        private PageBase _base { get; set; }
+        #endregion
+
+        #endregion
+    }
+}
