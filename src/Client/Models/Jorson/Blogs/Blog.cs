0d365d9c9f03be29fbfc7dbb5263cbd0d7fefeed
James Orson <jamesaorson@gmail.com>
2023-06-12 17:23:18 -0700

    BUG: Fix date issue

--- src/Client/Models/Jorson/Blogs/Blog.cs
+++ src/Client/Models/Jorson/Blogs/Blog.cs
@@ -5,7 +5,7 @@ namespace Client.Models.Jorson.Blogs
 {
     public class Blog<TId> : Model<TId>
     {
-        public DateTime Date { get; set; }
+        public String Date { get; set; }
         public string Title { get; set; }
         public BlogContent Content { get; set; }
         public bool IsHidden { get; set; }
